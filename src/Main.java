cb7da3b5e716f8704a6f6347bb114a75d058d9e1
AndreLopes30 <ferreiraandre833@gmail.com>
2026-05-14 23:57:35 -0300

    Primeiro exercício comentários

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/* Exercício 1 — Classe e Objetos
+ * Criação de uma classe Aluno com atributos e método de instância,
+ * praticando instanciação de objetos e orientação a objetos básica em Java.
+ */
+
 public class Main {
     public static void main(String[] args) {
         Alunos alunoNovo = new Alunos();
