c92aaee397117cfb5bc5ab7e0550dce3a62bec7b
AndreLopes30 <ferreiraandre833@gmail.com>
2026-05-14 23:54:22 -0300

    Primeiro exercício vF

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
         Alunos alunoNovo = new Alunos();
         alunoNovo.nota = 6;
         alunoNovo.nome = "André";
-        System.out.println(alunoNovo.nome, alunoNovo.situacao());
+        System.out.println(alunoNovo.nome + " - " + alunoNovo.situacao());
+        Alunos alunoNovo2 = new Alunos();
+        alunoNovo2.nota = 4;
+        alunoNovo2.nome = "Ferreira";
+        System.out.println(alunoNovo2.nome + " - " + alunoNovo2.situacao());
+        Alunos alunoNovo3 = new Alunos();
+        alunoNovo3.nota = 8;
+        alunoNovo3.nome = "Lopes";
+        System.out.println(alunoNovo3.nome + " - " + alunoNovo3.situacao());
     }
 }
 
