d93935249b589b466779e2f40ebc5861fe8d7221
AndreLopes30 <ferreiraandre833@gmail.com>
2026-05-15 23:50:48 -0300

    Exercicio2 v1

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,3 +1,25 @@
-public class Exercicio2 {
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Alunos> listaAlunos = new ArrayList<>();
+        Alunos alunoNovo = new Alunos();
+        for(Alunos aluno : listaAlunos) {
+            listaAlunos.append(aluno);
+            System.out.println(aluno.nome + " - " + aluno.situacao());
+        }
+    }
+}
+
+class Alunos {
+    String nome;
+    double nota;
+    public String situacao() {
+        if (nota >= 6) {
+            return "Aprovado";
+        }
+        else {
+            return "Reprovado";
+        }
+    }
 }
